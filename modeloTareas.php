bce3ed73c12ecb13092efceca82e1a70d69f27aa
jrodben <jaimerodriguezbenitez@iestorredelrey.es>
2024-04-18 17:01:51 +0200

    getTasklist method

--- modeloTareas.php
+++ modeloTareas.php
@@ -1,4 +1,20 @@
 <?php
 include('config.php');
 
+
+function getTasksList() {
+    global $conexionBD;
+    $sql = "SELECT * FROM tareas";
+    $stmt = $conexionBD->query($sql);
+    $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($tareas) {
+        return $tareas;
+    } else {
+        return array("message" => "No se encontraron tareas");
+    }
+}
+
+
+
 ?>
