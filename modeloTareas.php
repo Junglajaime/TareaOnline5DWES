97e1db7105831c1003c3d312c6aa43158ad3a9a9
jrodben <jaimerodriguezbenitez@iestorredelrey.es>
2024-04-18 17:02:29 +0200

    orderByDay method

--- modeloTareas.php
+++ modeloTareas.php
@@ -15,6 +15,26 @@ function getTasksList() {
     }
 }
 
+function getOrderedTasksListByDay($params) {
+    global $conexionBD;
+    $dia = isset($params['dia']) ? $params['dia'] : null;
+    if ($dia !== null) {
+        $sql = "SELECT * FROM tareas WHERE DATE(fecha) = :dia";
+        $stmt = $conexionBD->prepare($sql);
+        $stmt->execute(['dia' => $dia]);
+        $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($tareas) {
+            return $tareas;
+        } else {
+            return array("message" => "No se encontraron tareas para la fecha proporcionada");
+        }
+    } else {
+        return array("error" => "Fecha no proporcionada");
+    }
+}
+
+
 
 
 ?>
